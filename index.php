a8271f553e992b05ecd017652fda3107f75a3381
Nabil Ghedjati <nabil.ghedjati@gmail.com>
2017-03-02 15:13:15 +0100

    nouveau cv2017

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
 			<h1 id="grandTitre"><a href="#">Nabil.Ghedjati</a></h1>
 			<noscript id="noscriptDesktop">
 				Ce site nécessite l'activation de JavaScript.
-				<p><a class="downloadlink" href="assets/cv_nabil_ghedjati.pdf" type="application/pdf" target="_blank">Télécharger le CV</a></p>
+				<p><a class="downloadlink" href="assets/cv_nabil_ghedjati_2017.pdf" type="application/pdf" target="_blank">Télécharger le CV</a></p>
 			</noscript>
 		</section>
 
@@ -63,7 +63,7 @@
 					<h2 class="hw" id="hwMobile">Hello world.<span class='cursor'>█</span></h2>
 					<p id="msg">Bienvenue ! Je suis un jeune étudiant actuellement en reconversion dans le monde du Web,</br> plein d'idées et de motivation. Consultez mon parcours et n'hésitez pas à me contacter.
 					</p>
-					<p><a class="downloadlink" href="assets/cv_nabil_ghedjati.pdf" type="application/pdf" target="_blank">Télécharger le CV</a></p>
+					<p><a class="downloadlink" href="assets/cv_nabil_ghedjati_2017.pdf" type="application/pdf" target="_blank">Télécharger le CV</a></p>
 					<noscript>
 						Ce site nécessite l'activation de JavaScript.
 					</noscript>
