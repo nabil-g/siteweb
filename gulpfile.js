3497d1906e101326549a625b21fb4a0e2445ceda
Nabil <nabil.ghedjati@gmail.com>
2020-01-22 22:58:45 +0100

    format js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,49 @@
-const gulp = require('gulp');
-const plugins = require('gulp-load-plugins')();
-const del = require('del');
-const runSequence = require('run-sequence');
-const replace = require('gulp-replace');
+const gulp = require("gulp");
+const plugins = require("gulp-load-plugins")();
+const del = require("del");
+const runSequence = require("run-sequence");
+const replace = require("gulp-replace");
 
 console.dir(plugins); // pour récuperer le nom des plugins dans l'objet plugins
 
-const formEndpoint = process.env.FORM_ENDPOINT || '';
-
+const formEndpoint = process.env.FORM_ENDPOINT || "";
 
 // vider le dossier dist sauf le sous-dossier assets
-gulp.task('clean:dist', function () {
-  del(['dist/**/*', 'dist/*','!dist/assets', '!dist/assets/*']);
+gulp.task("clean:dist", function() {
+  del(["dist/**/*", "dist/*", "!dist/assets", "!dist/assets/*"]);
 });
 
 // concatener les fichiers sources dans l'index.html
-gulp.task('useref', function () {
-  return gulp.src('src/index.html')
+gulp.task("useref", function() {
+  return gulp
+    .src("src/index.html")
     .pipe(plugins.useref())
-    .pipe(plugins.if('*.js',plugins.uglify())) // minifier seulement les scripts js
-    .pipe(plugins.if('*.js',plugins.replace('FORM_ENDPOINT', formEndpoint)))
-    .pipe(plugins.if('*.css',plugins.cleanCss({compatibility: 'ie8'}))) // minifier seulement les scripts css
-    .pipe(gulp.dest('dist'));
+    .pipe(plugins.if("*.js", plugins.uglify())) // minifier seulement les scripts js
+    .pipe(plugins.if("*.js", plugins.replace("FORM_ENDPOINT", formEndpoint)))
+    .pipe(plugins.if("*.css", plugins.cleanCss({ compatibility: "ie8" }))) // minifier seulement les scripts css
+    .pipe(gulp.dest("dist"));
 });
 
 // copier les fichiers qui n'ont pas besoin de changement
-gulp.task('noChange', function () {
-  return gulp.src(['src/+(assets|error|php|lib)/**','!src/assets/*.jpg','src/*.css'])
-  .pipe(gulp.dest('dist'));
+gulp.task("noChange", function() {
+  return gulp
+    .src(["src/+(assets|error|php|lib)/**", "!src/assets/*.jpg", "src/*.css"])
+    .pipe(gulp.dest("dist"));
 });
 
 // compresser les jpeg
-gulp.task('compressJpg', function () {
-  return gulp.src('src/assets/*.jpg')
-    .pipe(plugins.cache(plugins.imagemin([
-      plugins.imagemin.jpegtran({progressive:true})
-    ])))
-    .pipe(gulp.dest('dist/assets/'));
+gulp.task("compressJpg", function() {
+  return gulp
+    .src("src/assets/*.jpg")
+    .pipe(
+      plugins.cache(
+        plugins.imagemin([plugins.imagemin.jpegtran({ progressive: true })])
+      )
+    )
+    .pipe(gulp.dest("dist/assets/"));
 });
 
-
 // exécuter des taches, d'abord clean:dist puis useref et noChange en meme temps
-gulp.task('default', ['clean:dist'] ,function () {
-  runSequence(['useref','compressJpg','noChange']);
+gulp.task("default", ["clean:dist"], function() {
+  runSequence(["useref", "compressJpg", "noChange"]);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
-const webpack = require('webpack');
-const webpackMerge = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const StyleLintPlugin = require('stylelint-webpack-plugin');
+const webpack = require("webpack");
+const webpackMerge = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const StyleLintPlugin = require("stylelint-webpack-plugin");
 const path = require("path");
 const modeConfig = env => require(`./build-utils/webpack.${env}`)(env);
 const presetConfig = require("./build-utils/loadPresets");
@@ -12,35 +12,32 @@ module.exports = ({ mode, presets } = { mode: "production", presets: [] }) => {
 
   return webpackMerge(
     {
-
       mode,
       entry: {
-        main: path.join(__dirname, './src/index.js')
+        main: path.join(__dirname, "./src/index.js")
       },
       module: {
         noParse: /\.elm$/,
         rules: [
           {
             test: /\.(eot|ttf|woff|woff2|svg)$/,
-            use: 'file-loader?publicPath=../../&name=assets/css/[hash].[ext]',
-          },
+            use: "file-loader?publicPath=../../&name=assets/css/[hash].[ext]"
+          }
         ]
       },
       plugins: [
         new HtmlWebpackPlugin({
-          template: 'src/index.html',
-          inject: 'body',
-          filename: 'index.html',
+          template: "src/index.html",
+          inject: "body",
+          filename: "index.html"
         }),
 
         //new StyleLintPlugin(),
 
-        new CopyWebpackPlugin([
-          { from: 'src/assets/favicon.png' }
-        ]),
+        new CopyWebpackPlugin([{ from: "src/assets/favicon.png" }])
       ]
     },
     modeConfig(mode),
-    presetConfig({ mode, presets }),
-  )
+    presetConfig({ mode, presets })
+  );
 };
